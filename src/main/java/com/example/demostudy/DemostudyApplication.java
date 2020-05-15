a1ddbf62abb1e7156303e1d58699b7b313e3c227
simonfung.ftt@gmail.com <simonfung.ftt@gmail.com>
2020-05-16 01:34:07 +0800

    First commit

---
+++ src/main/java/com/example/demostudy/DemostudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemostudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemostudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demostudy/DemostudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemostudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
